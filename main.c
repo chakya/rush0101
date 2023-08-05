e64717440519ba4c0d0efd6cfc482bedb50e3c85
Wendy Huang <whuang@c2r3s8.42singapore.sg>
2023-08-05 20:47:13 +0800

    main.c

--- main.c
+++ main.c
@@ -1,19 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main1.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cwijaya <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 15:23:46 by cwijaya           #+#    #+#             */
-/*   Updated: 2023/08/05 15:23:47 by cwijaya          ###   ########.fr       */
+/*   Updated: 2023/08/05 20:44:24 by whuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /*	v = 0: colup; v = 1: coldown; v = 2: rowleft; v = 3: rowright  */
+
+#include <unistd.h>
+
 int	**create_array(int x, int y);
 void	print_array(int **array, int x, int y);
 
-int	parse_for_view(int argc, char **argv, int **view, int size)
+int	parse_for_view(char **argv, int **view, int size)
 {
 	int	i;
 	int	v;
@@ -21,8 +24,6 @@ int	parse_for_view(int argc, char **argv, int **view, int size)
 
 	i = 0;
 	v = 0;
-	if (argc != 2)
-		return (0);
 	while (argv[1][i] != '\0' && v < size)
 	{
 		c = 0;
@@ -36,6 +37,27 @@ int	parse_for_view(int argc, char **argv, int **view, int size)
 	}
 	return (1);
 }
+int	param_error(char* argv)
+{
+	int	i;
+
+	i = 0;
+	while (argv[i] != '\0')
+	{
+		if (i % 2 == 1)
+		{
+			if (argv[i] != ' ')
+				return (1);
+		}
+		else
+			if (argv[i] < '1' || argv[i] > '4')
+				return (1);
+		i++;
+	}
+	if (i != 31)
+		return (1);
+	return (0);
+}
 
 int	main(int argc, char **argv)
 {
@@ -43,7 +65,12 @@ int	main(int argc, char **argv)
 	int	size;
 
 	size = 4;
+	if ((argc != 2) || param_error(argv[1]))
+	{
+		write(1, "Error\n", 6);
+		return (-1);
+	}
 	view = create_array(size, size);
-	parse_for_view(argc, argv, view, size);
+	parse_for_view(argv, view, size);
 	print_array(view, size, size);
 }
